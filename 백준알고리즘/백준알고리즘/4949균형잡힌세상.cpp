e1f41a3f653ba3320cf401b6d8573486eb911ec6
OHMIJUNG <ael8548@ajou.ac.kr>
2020-07-26 04:01:01 +0900

    final!! success!!

--- 백준알고리즘/백준알고리즘/4949균형잡힌세상.cpp
+++ 백준알고리즘/백준알고리즘/4949균형잡힌세상.cpp
@@ -2,90 +2,82 @@
 #include<stack>
 #include<string>
 #include<iostream>
-
 using namespace std;
 
 stack<char> s;
 
-bool check(string string) {
-	int len = string.length();
-	int flag = 0;
-	int k = 0;
-
-	while (1) {
-		if (string[k] == '(' || string[k] == '[' || string[k] == ')' || string[k] == ']')
+bool check(string st) {
+	int len = st.length();
+	for (int i = 0; i < len; i++) {
+		if (st[i] == '(' || st[i] == '[')
 		{
-			flag = 1;//괄호있는 문장
-			break;
+			s.push(st[i]);
+			continue;
 		}
-		k++;
-		if (k == len - 1)
-			break;
-	}
-
-	if (flag == 1) {
-		int start;
-		for (int i = 0; i < len; i++) {
-			if (string[i] == '(' || string[i] == '[')
+		else if (st[i] == ')') {
+			if (!s.empty() && s.top() == '(')
 			{
-				s.push(string[i]);
-				start = i;
-				break;
+				s.pop();
+				continue;
 			}
-		}//첫번째 괄호 넣어주기(empty에서 에러 안나게 하기 위해)
-
-		for (int i = start + 1; i < len; i++) {
-			if (!s.empty()) {
-				if (s.top() == '(' && string[i] == ')')
-				{
-					s.pop();
-					continue;
-				}
-				else if (s.top() == '[' && string[i] == ']') {
-					s.pop();
-					continue;
-
-				}
-				else if (s.top() == '(') {
-					if (string[i] == ']' || string[i] == '[' || string[i] == '(')
-					{
-						s.push(string[i]);
-						continue;
-					}
-
-				}
-				else if (s.top() == '[') {
-					if (string[i] == '(' || string[i] == '[' || string[i] == ')')
-					{
-						s.push(string[i]);
-						continue;
-					}
-				}
+			else if (s.empty()) {
+				return false;
+			}
+			else
+			{
+				s.push(st[i]);
+				continue;
 			}
 		}
+		else if (st[i] == ']')
+		{
+			if (!s.empty() && s.top() == '[')
+			{
+				s.pop();
+				continue;
+			}
+			else if (s.empty()) {
+				return false;
+			}
+			else
+			{
+				s.push(st[i]);
+				continue;
+			}
+		}
+		else
+			continue;
+
 	}
-	else
-		return true;
 
 	if (s.empty())
 		return true;
 	else
 		return false;
-
 }
-int main() {
-	while (1) {
-		string string;
-		getline(cin, string);
-		if (string[0] == '.') {
-			printf("yes\n");
-			return 0;
-		}
 
-		if (check(string))
+int main() {
+	int i = 0;
+	string st;
+
+	while (1) {
+		st = "";
+		getline(cin, st);
+		if (st.length() == 1 && st[0] == '.')
+			break;
+
+		if (check(st))
 			printf("yes\n");
 		else
 			printf("no\n");
+
+
+		if (!s.empty()) {
+			while (!s.empty())
+				s.pop();
+		}
 	}
+
+	return 0;
 
 }

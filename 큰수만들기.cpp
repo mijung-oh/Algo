b7d1be9a5969be2319003f4dc70022185bc20056
mijung-oh <ks03102@naver.com>
2020-09-21 16:10:22 +0900

    큰수만들기 ing

--- 큰수만들기.cpp
+++ 큰수만들기.cpp
@@ -1,45 +1,45 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-string solution(string number, int k) {
+string solution(string number, int k)
+{
 	string answer = "";
+
 	int make, max_index;
 	make = number.size() - k;
 	int max = -1;
 	int size = number.size();
+	max_index = 0;
 
-	while (1) {
+	while (1)
+	{
 		max = -1;
 		if (make == number.size())
 		{
 			answer += number;
 			break;
 		}
-		for (int i=0; i < number.size() - make + 1; i++) {
+
+		for (int i = 0; i < number.size() - make + 1; i++)
+		{
 			string s = number.substr(i, make);
 			//최대 정수 찾기
-			if (max < stoi(s)) {
+			if (max < stoi(s))
+			{
 				max = stoi(s);
 				max_index = i;
 			}
 			else if (max == stoi(s))
 				continue;
 		}
+
 		answer += number[max_index];
 		make--;
-		number = number.substr(max_index+1);
+		number = number.substr(max_index + 1);
 		if (make == 0)
 			break;
-
 	}
 	return answer;
-}
-int main() {
-	string s = "10000";
-	int k = 2;
-	string result = solution(s, k);
-	cout << result;
 }

985dc2febb9b324e144c14b16d4562afe1ae286b
mijung-oh <ks03102@naver.com>
2020-09-25 17:26:04 +0900

    큰수만들기 +9/10

--- 큰수만들기.cpp
+++ 큰수만들기.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 using namespace std;
 
-string solution(string number, int k) {
+string solution(string number, int k)
+{
 	string answer = "";
-	int mk = number.length() - k;//만들어야 하는 문자열의 길이
-	int maxIDX = 0;//큰 값의 index
+	int mk = number.length() - k; //만들어야 하는 문자열의 길이
+	int maxIDX = 0;				  //큰 값의 index
 
-	
-
-	while (1) {
+	while (1)
+	{
 		string s = number.substr(0, 1);
 		maxIDX = 0;
-		for (int i = 1; i < number.length(); i++) {
+		for (int i = 1; i < number.length(); i++)
+		{
 			string s2 = number.substr(i, 1);
-			if (s < s2 && number.length()-i >= mk) {
+			if (s < s2 && number.length() - i >= mk)
+			{
 				s = s2;
 				maxIDX = i;
 			}
@@ -24,13 +26,14 @@ string solution(string number, int k) {
 		mk--;
 		answer += number[maxIDX];
 		number = number.substr(maxIDX + 1);
-		
-		if (mk == 0) break;
 
+		if (mk == 0)
+			break;
 	}
 	return answer;
 }
-int main() {
+int main()
+{
 	string string = "985632165";
 	int k = 5;
 	cout << solution(string, k);

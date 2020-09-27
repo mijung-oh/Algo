f9375fdde5468b220beaf51c145e8a7588de76cf
mijung-oh <ks03102@naver.com>
2020-09-27 23:10:15 +0900

    ing

---
+++ 여행경로.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(vector<vector<string>> tickets) {
+    vector<string> answer;
+    
+    vector<vector<<string>>g;//그래프
+    for(int i=0; i<tickets.size(); i++){
+        
+    }
+    //알파벳 앞서는 순서로 하기 위해 정렬
+    for(int i=0; i<g.size(); i++){
+        sort(g[i].begin(), g[i].end());
+    }
+    
+    
+    return answer;
+}

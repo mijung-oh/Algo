2c96cbdd84a85e1e60bb0b7f38acf03fcbeba0e0
mijung-oh <ks03102@naver.com>
2020-10-05 01:52:00 +0900

    2178 미로찾기 success

--- 2178.cpp
+++ 2178.cpp
@@ -1,54 +1,57 @@
 #include <stdio.h>
+#include <queue>
 
 using namespace std;
+queue<pair<int,int>>q;
+
+//남북동서
+int X[4] = { 1,-1,0,0 };
+int Y[4] = { 0,0,1,-1 };
+
 int map[101][101] = { 0, };
-
-//우상하좌
-//int X[4] = { 0,-1,1,0 };
-//int Y[4] = { 1,0,0,-1 };
-
-int X[4] = { 0,0,1,-1 };
-int Y[4] = { 1,-1,0,0 };
-
+int cnt = 0;
 int n, m;
-int cnt = 987654321;
 
-void dfs(int x, int y, int go, int visited[101][101]) {
-	//마지막에 온 경우
-	if (x == n-1 && y == m-1) {
-		if (go + 1 < cnt) cnt = go + 1;
-		return;
-	}
+void bfs(int x,int y) {
 
-	go++;
+	q.push(make_pair(0, 0));
 
-	for (int i = 0; i < 4; i++) {
-		int nx = x + X[i];
-		int ny = y + Y[i];
+	while (!q.empty()) {
+		int first = q.front().first;
+		int second = q.front().second;
+		q.pop();
 
+		for (int i = 0; i < 4; i++) {
+			int nx = first + X[i];
+			int ny = second + Y[i];
 
-		if (nx >= 0 && ny >= 0 && nx < n && ny < m && visited[nx][ny] != 1 && map[nx][ny] != 0)
-		{
-			visited[nx][ny] = 1;
-			dfs(nx, ny, go, visited);
-			visited[nx][ny] = 0;
+			if (nx >= 0 && ny >= 0 && nx < n && ny < m &&
+				map[nx][ny] == 1)
+			{			
+				q.push(make_pair(nx, ny));
+				map[nx][ny] = map[first][second] + 1;
+			}
 		}
+		
 	}
 	return;
 }
 
 int main() {
 	scanf("%d %d", &n, &m);
-	int visited[101][101] = { 0, };
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			scanf("%1d", &map[i][j]);
 		}
 	}
-	
+	bfs(0, 0);
+	/*for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d ", map[i][j]);
+		}
+		printf("\n");
+	}*/
 
-	dfs(0, 0, 0, visited);
-
-	printf("%d\n", cnt);
+	printf("%d", map[n-1][m-1]);
 }
